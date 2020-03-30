ec846adebee828f7c165963b567730d1b6619b70
Tedla B <mekdi.td87@outlook.com>
2020-03-30 05:34:16 -0500

    rearranged index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, my name is Bingo!</h1>
+      <p>I love to travel and my three dream destinations are </p>
+      <ul>
+        <li>Brazil</li>
+        <li>United Kingdom</li>
+        <li>Australia</li>
+      </ul>
     </div>
   );
 }
